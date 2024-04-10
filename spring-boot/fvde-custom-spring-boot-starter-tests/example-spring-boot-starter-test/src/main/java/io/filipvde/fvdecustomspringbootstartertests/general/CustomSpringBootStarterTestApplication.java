50da25fa7b6b78a296bdbbeec82accba243c8122
Filip Vanden Eynde <47209284+filipve1994@users.noreply.github.com>
2024-04-10 20:08:33 +0100

    add example spring boot starter folders

---
+++ spring-boot/fvde-custom-spring-boot-starter-tests/example-spring-boot-starter-test/src/main/java/io/filipvde/fvdecustomspringbootstartertests/general/CustomSpringBootStarterTestApplication.java
@@ -0,0 +1,13 @@
+package io.filipvde.fvdecustomspringbootstartertests.general;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomSpringBootStarterTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomSpringBootStarterTestApplication.class, args);
+	}
+
+}
---
+++ spring-boot/fvde-custom-spring-boot-starter-tests/example-spring-boot-starter-test/src/test/java/io/filipvde/fvdecustomspringbootstartertests/general/CustomSpringBootStarterTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.filipvde.fvdecustomspringbootstartertests.general;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomSpringBootStarterTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
